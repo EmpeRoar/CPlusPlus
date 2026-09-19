25ea7fd7ef5f8526d5c752f471a8a99c3602530a
Julius Bacosa <juliusbacosa@gmail.com>
2026-09-19 09:48:23 +0800

    ...

--- hello-world/hello-world.cpp
+++ hello-world/hello-world.cpp
@@ -1,6 +1,42 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, \n World!" << endl;
+    cout << "This is a simple C++ program." << endl;
+    cout << 3 << endl;
+    string cool;
+    cin >> cool;
+    cout << 3 + 3 << endl;
+    cout << 2 * 5 << endl;
+    cout << cool << endl;
+
+    int day = 4;
+
+    switch (day) {
+        case 1:
+            cout << "Monday" << endl;
+            break;
+        case 2:
+            cout << "Tuesday" << endl;
+            break;
+        case 3:
+            cout << "Wednesday" << endl;
+            break;
+        case 4:
+            cout << "Thursday" << endl;
+            break;
+        case 5:
+            cout << "Friday" << endl;
+            break;
+        case 6:
+            cout << "Saturday" << endl;
+            break;
+        case 7:
+            cout << "Sunday" << endl;
+            break;
+        default:
+            cout << "Invalid day" << endl;
+    }
     return 0;
 }
