a86ca78b25f9bc56b49e1acb693a16ce05732e6c
Julius Bacosa <juliusbacosa@gmail.com>
2026-09-20 16:17:57 +0800

    ...

--- todo/main.cpp
+++ todo/main.cpp
@@ -1,31 +1,78 @@
 #include <iostream>
-#include <array>
-#include <string>
+#include <optional>
 
 using namespace std;
 
-void displayList(array<string, 100> todos, int todoCount) {
-    cout << "Todo List:" << endl;
-    for(int i = 0; i < todoCount; i++) {
-        cout << i + 1 << ". " << todos[i] << endl;
-    }
+void displayOptions()
+{
+    cout << "\nSelect what you want to do!" << endl;
+    cout << "1. Add a todo item" << endl;
+    cout << "2. Display todo list" << endl;
+    cout << "3. Exit" << endl;
+    cout << "Please enter your choice: ";
 }
 
-int main() {
+optional<int> getUserInput()
+{
+    int choice;
 
-   array<string, 100> todos;
-   int todoCount = 0;
+    if (cin >> choice)
+    {
+        return choice;
+    }
 
-   while(true) {
-    cout << "Enter a todo Item: ";
-    string todo;
-    getline(cin, todo); // To consume the newline character after the first input
-    todos[todoCount] = todo;
-    todoCount++;
+    // Clear the error state
+    cin.clear();
 
+    // Remove invalid input
+    cin.ignore(10000, '\n');
 
-    displayList(todos, todoCount);
-   }
+    return nullopt;
+}
+
+void displayTodoList()
+{
+    // Placeholder for displaying the todo list
+    cout << "Displaying the todo list..." << endl;
+    // Logic to display the todo list goes here
+}
+int main()
+{
+    optional<int> choice = nullopt;
+
+    while (true)
+    {
+        displayOptions();
+
+        choice = getUserInput();
+
+        // No valid input
+        if (!choice.has_value())
+        {
+            cout << "Invalid input. Please enter a number." << endl;
+            continue;
+        }
+
+        switch (choice.value())
+        {
+            case 1:
+                cout << "You chose to add a todo item." << endl;
+                // Add todo logic
+                break;
+
+            case 2:
+                cout << "You chose to display the todo list." << endl;
+                displayTodoList();
+                break;
+
+            case 3:
+                cout << "Exiting the program." << endl;
+                return 0;
+
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+        }
+    }
 
     return 0;
 }
